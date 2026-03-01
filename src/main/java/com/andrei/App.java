27ade15a1c0cf1c4f2cd228644bd8f9dc27309bd
Andrei Pirone <andrei.pirone@gmail.com>
2026-03-01 17:14:37 +0200

    added a new endpoint

--- src/main/java/com/andrei/App.java
+++ src/main/java/com/andrei/App.java
@@ -1,9 +1,8 @@
 package com.andrei;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 
 public class App {
     public static void main(String[] args) {
@@ -15,11 +14,22 @@ public class App {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Accepted connection from " + clientSocket.getRemoteSocketAddress());
 
-            InputStream inputStream = clientSocket.getInputStream();
-            OutputStream outputStream = clientSocket.getOutputStream();
-            String message = "HTTP/1.1 200 OK\r\n\r\n";
-            outputStream.write(message.getBytes());
-            outputStream.flush();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String requestURL = in.readLine().split(" ")[1];
+
+            //in.lines().forEach(System.out::println);
+
+            if(requestURL.equals("/")){
+                out.write("HTTP/1.1 200 OK\r\n\r\n");
+            } else if (requestURL.startsWith("/echo/")) {
+                String text = requestURL.substring(6);
+                out.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + text.length() +"\r\n\r\n" + text);
+            } else {
+                out.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
